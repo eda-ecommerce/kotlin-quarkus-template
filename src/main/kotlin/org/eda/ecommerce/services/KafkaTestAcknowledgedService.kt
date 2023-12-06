8793770e5bc8cd10c78ff3ea927e418b45123de4
OliverWich <kontakt@oliver-wich.de>
2023-12-06 11:00:31 +0100

    fix: kafka tests

--- src/main/kotlin/org/eda/ecommerce/services/KafkaTestAcknowledgedService.kt
+++ src/main/kotlin/org/eda/ecommerce/services/KafkaTestAcknowledgedService.kt
@@ -4,8 +4,6 @@ import jakarta.enterprise.context.ApplicationScoped
 import jakarta.inject.Inject
 import org.eclipse.microprofile.reactive.messaging.Channel
 import org.eclipse.microprofile.reactive.messaging.Emitter
-import org.eclipse.microprofile.reactive.messaging.Message
-import java.util.concurrent.CompletableFuture
 
 @ApplicationScoped
 class KafkaTestAcknowledgedService {
@@ -14,19 +12,6 @@ class KafkaTestAcknowledgedService {
     private lateinit var testAcknowledgedEmitter: Emitter<String>
 
     fun emitTestMessage(message: String) {
-        testAcknowledgedEmitter.send(
-            Message.of(message)
-                .withAck {
-                    println("Acked")
-                    CompletableFuture.completedFuture(
-                        null
-                    )
-                }
-                .withNack { throwable: Throwable? ->
-                    println("Nacked")
-                    CompletableFuture.completedFuture(
-                        null
-                    )
-                })
+        testAcknowledgedEmitter.send(message)
     }
 }
--- src/test/kotlin/org/eda/ecommerce/integration/ReEmitterTest.kt
+++ src/test/kotlin/org/eda/ecommerce/integration/ReEmitterTest.kt
@@ -1,62 +1,81 @@
 package org.eda.ecommerce.integration
 
-import io.quarkus.test.common.QuarkusTestResource
 import io.quarkus.test.junit.QuarkusTest
-import io.quarkus.test.kafka.InjectKafkaCompanion
-import io.quarkus.test.kafka.KafkaCompanionResource
-import io.smallrye.reactive.messaging.kafka.companion.ConsumerTask
-import io.smallrye.reactive.messaging.kafka.companion.KafkaCompanion
+import io.smallrye.common.annotation.Identifier
+import jakarta.inject.Inject
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.consumer.ConsumerRecords
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.kafka.common.serialization.StringSerializer
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import java.time.Duration
+import java.util.*
 
 
 @QuarkusTest
-@QuarkusTestResource(KafkaCompanionResource::class)
 class ReEmitterTest {
 
-    @InjectKafkaCompanion
-    lateinit var companion: KafkaCompanion
+    @Inject
+    @Identifier("default-kafka-broker")
+    lateinit var kafkaConfig: Map<String, Any>
 
+    lateinit var testProducer: KafkaProducer<String, String>
+    lateinit var testAcknowledgedConsumer: KafkaConsumer<String, String>
 
     @BeforeEach
-    fun recreateTestedTopics() {
-        companion.topics().delete("test")
-        companion.topics().create("test", 1)
-        companion.topics().delete("test-acknowledged")
-        companion.topics().create("test-acknowledged", 1)
+    fun setupKafkaHelpers() {
+        testAcknowledgedConsumer = KafkaConsumer(consumerConfig(), StringDeserializer(), StringDeserializer())
+        testProducer = KafkaProducer(kafkaConfig, StringSerializer(), StringSerializer())
     }
+
+    fun consumerConfig(): Properties {
+        val properties = Properties()
+        properties.putAll(kafkaConfig)
+        properties[ConsumerConfig.GROUP_ID_CONFIG] = "test-group-id"
+        properties[ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG] = "true"
+        properties[ConsumerConfig.AUTO_OFFSET_RESET_CONFIG] = "earliest"
+        return properties
+    }
+
+    @AfterEach
+    fun tearDown() {
+        testProducer.close()
+        testAcknowledgedConsumer.close()
+    }
+
 
     @Test
     fun testReEmitterOnce() {
-        companion.produce(ByteArray::class.java).fromRecords(
-            ProducerRecord("test", "A String Value".toByteArray())
-        )
+        testAcknowledgedConsumer.subscribe(listOf("test-acknowledged"))
 
-        val testAcknowledgementConsumer: ConsumerTask<String, String> = companion.consumeStrings().fromTopics("test", 1)
+        testProducer.send(ProducerRecord("test", "A String Value"))
 
-        testAcknowledgementConsumer.awaitCompletion()
+        val records: ConsumerRecords<String, String> = testAcknowledgedConsumer.poll(Duration.ofMillis(10000))
 
-        Assertions.assertEquals(1, testAcknowledgementConsumer.count())
-        Assertions.assertEquals("A String Value", testAcknowledgementConsumer.firstRecord.value())
+        Assertions.assertEquals(1, records.records("test-acknowledged").count())
+        Assertions.assertEquals("A String Value", records.records("test-acknowledged").iterator().next().value())
     }
 
     @Test
     fun testReEmitterMultiple() {
-        companion.produce(ByteArray::class.java).fromRecords(
-            ProducerRecord("test", "first".toByteArray()),
-            ProducerRecord("test", "second".toByteArray()),
-            ProducerRecord("test", "third".toByteArray())
-        )
+        testAcknowledgedConsumer.subscribe(listOf("test-acknowledged"))
 
-        val testAcknowledgementConsumer: ConsumerTask<String, String> = companion.consumeStrings().fromTopics("test", 3)
+        testProducer.send(ProducerRecord("test", "first"))
+        testProducer.send(ProducerRecord("test", "second"))
+        testProducer.send(ProducerRecord("test", "third"))
 
-        testAcknowledgementConsumer.awaitCompletion()
+        val records: ConsumerRecords<String, String> = testAcknowledgedConsumer.poll(Duration.ofMillis(10000))
 
-        Assertions.assertEquals(3, testAcknowledgementConsumer.count())
-        Assertions.assertEquals("first", testAcknowledgementConsumer.records[0].value())
-        Assertions.assertEquals("second", testAcknowledgementConsumer.records[1].value())
-        Assertions.assertEquals("third", testAcknowledgementConsumer.records[2].value())
+        Assertions.assertEquals(3, records.count())
+        val values = records.records("test-acknowledged").iterator().asSequence().toList().map { it.value() }
+        Assertions.assertEquals("first", values[0])
+        Assertions.assertEquals("second", values[1])
+        Assertions.assertEquals("third", values[2])
     }
 }
